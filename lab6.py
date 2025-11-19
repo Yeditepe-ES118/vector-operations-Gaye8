2fc74ceccbe3d0265b7295f14d592d4951d94cc2
Gaye8 <gaye.akuz@std.yeditepe.edu.tr>
2025-11-19 16:46:50 +0300

    added untracked files

---
+++ lab6.py
@@ -0,0 +1,13 @@
+import numpy as np 
+def arrays():
+    array1 =np.array([-2.33, 100, 20, 33.2])
+    array2 =np.array ([30, 12, 2, 70.2]),[98.01, 4, 0, 7]                
+    array3 =np.arange ([2, 12, 2]) 
+    array4 =np.arange ([2.0, -21, -10])
+    array5 =np.linspace ([0, 1, 4])
+    array6 =np.ones((3, 4))          
+    array7 =np.zeros((2, 3))  
+    array8 =np.eye(3)
+    array9 =np.diag(np.ones(2,) ,-1)
+    array10 =np.array [np.zeros((4,)), np.ones((4,)), 2*np.ones((4,))]
+    return array1, array2, array3, array4, array5, array6, array7, array8, array9, array10
---
+++ untitled3.py
@@ -0,0 +1,9 @@
+import numpy as np 
+def arrays():
+    array1 =np.array([-2.33, 100, 20, 33.2])
+    array2 =np.array ([30, 12, 2, 70.2]),[98.01, 4, 0, 7]                
+    array3 =np.arange ([2, 12, 2]) 
+    array4 =np.arange ([2.0, -21, -10])
+    array5 =np.linspace ([0, 1, 4])
+    array6 =np.ones((3, 4))          
+    array7 =np.zeros((2, 3))             
